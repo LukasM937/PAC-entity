df1ffb30c11097dfb057dfd0b5283ade2b4de64c
Lukas Mührke <lukas.muehrke@tuhh.de>
2020-01-09 19:51:03 +0100

    New

--- src/Main.c
+++ src/Main.c
@@ -4,8 +4,30 @@
 
 #include "Pac-entity.h"
 
+typedef struct player
+{
+    int posX;
+    int posY;
+} player;
+
+// void move(void* arg, SDL_Point& points)
+// {
+//     player* one = (player*)arg;
+//     one->posX = WINDOW_WIDTH/2;
+//     one->posY = WINDOW_HEIGHT/2;
+
+//     points = {{one->posX - 50, one->posY - 50},
+//             {one->posX + 50, one->posY - 50},
+//             {one->posX + 50, one->posY + 50},
+//             {one->posX - 50, one->posY + 50}
+//     };
+// }
+
 int main(void)
 {
+    player* one = malloc(sizeof(player));
+    // SDL_Point* points[POINTS_COUNT];
+
     SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window * window = NULL;
@@ -14,7 +36,7 @@ int main(void)
     SDL_Surface * image_surface = NULL;
 
     SDL_Renderer* renderer = NULL;
-    SDL_Event quitEvent;
+    SDL_Event event;
     int quit = 0;
     if(SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer) == 0)
     {
@@ -28,6 +50,7 @@ int main(void)
     
     while (quit == 0)
     {
+        //move(one, points);
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
         SDL_RenderClear(renderer);
 
@@ -35,15 +58,33 @@ int main(void)
         SDL_RenderDrawLines(renderer, points, POINTS_COUNT);
         SDL_RenderPresent(renderer);
 
-        while (SDL_PollEvent(&quitEvent)){
-            if (quitEvent.type == SDL_QUIT){
-                quit = 1;
-            }
-            if (quitEvent.type == SDL_KEYDOWN){
-                quit = 1;
-            }
-            if (quitEvent.type == SDL_MOUSEBUTTONDOWN){
-                quit = 1;
+        while (SDL_PollEvent(&event))
+        {
+
+            switch(event.type )
+            {
+            /* Look for a keypress */
+            case SDL_KEYDOWN:
+                /* Check the SDLKey values and move change the coords */
+                switch( event.key.keysym.sym ){
+                    case SDLK_LEFT:
+                        one->posX -= 1;
+                        break;
+                    case SDLK_RIGHT:
+                        one->posX  += 1;
+                        break;
+                    case SDLK_UP:
+                        one->posY  -= 1;
+                        break;
+                    case SDLK_DOWN:
+                        one->posY += 1;
+                        break;
+                    case SDLK_ESCAPE:
+                        quit =1;
+                        break;
+                    default:
+                        break;
+                }
             }
         }
     }
--- src/Pac-entity.h
+++ src/Pac-entity.h
@@ -2,7 +2,7 @@
 #define WINDOW_WIDTH 200
 #define WINDOW_HEIGHT 200
 
-static SDL_Point points[POINTS_COUNT] = {
+SDL_Point points[POINTS_COUNT] = {
     {335, 230},
     {315, 240},
     {335, 250},
