ce2f0eabc3f28270abf0680fa1a1c687ca912866
Lukas Mührke <lukas.muehrke@tuhh.de>
2020-01-09 11:39:09 +0100

    a few new Folders

---
+++ src/Pac-entity.h
@@ -0,0 +1,11 @@
+#define POINTS_COUNT 4
+#define WINDOW_WIDTH 200
+#define WINDOW_HEIGHT 200
+
+static SDL_Point points[POINTS_COUNT] = {
+    {335, 230},
+    {315, 240},
+    {335, 250},
+    {320, 200}
+};
+
