f3e93ce2c4907b27bbda2e620d66af2ca907386d
Lukas Mührke <lukas.muehrke@tuhh.de>
2020-01-13 07:54:53 +0100

    a few changes regarding the png files

--- src/Main.c
+++ src/Main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_render.h>
+#include <SDL_timer.h>
+#include </Users/lmuehrke/Documents/GitHub/PAC-entity/sdl2/2.0.10/include/SDL2/SDL_image.h>
 
 #include "Pac-entity.h"
+//#include "assets.h"
 
 typedef struct player
 {
@@ -26,16 +30,56 @@ typedef struct player
 int main(void)
 {
     player* one = malloc(sizeof(player));
-    // SDL_Point* points[POINTS_COUNT];
 
     SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window * window = NULL;
 
-    SDL_Surface * window_surface = NULL;
+    // SDL_Surface * window_surface = NULL;
     SDL_Surface * image_surface = NULL;
 
-    SDL_Renderer* renderer = NULL;
+    // create a renderer, which sets up the graphics hardware
+    Uint32 render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, render_flags);
+    if (!renderer)
+    {
+      printf("error creating renderer: %s\n", SDL_GetError());
+      SDL_DestroyWindow(window);
+      SDL_Quit();
+      return 1;
+    }
+
+    SDL_Surface* surface = IMG_Load("assets/PAC-entity_frameless.png");
+    if (!surface)
+    {
+        printf("error creating surface\n");
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    if (!tex)
+    {
+        printf("error creating texture: %s\n", SDL_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // clear the window
+    SDL_RenderClear(renderer);
+    
+    // draw the image to the window
+    SDL_RenderCopy(renderer, tex, NULL, NULL);
+    SDL_RenderPresent(renderer);
+
+    // wait a few seconds
+    SDL_Delay(5000);
+
     SDL_Event event;
     int quit = 0;
     if(SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer) == 0)
@@ -43,7 +87,7 @@ int main(void)
         quit = 0;
     }
 
-    window_surface = SDL_GetWindowSurface(window);
+    //window_surface = SDL_GetWindowSurface(window);
 
     SDL_UpdateWindowSurface(window);
 
